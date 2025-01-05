2e6a308973028e49ac24888738033a397093377c
Solace00 <nkumar301402@gmail.com>
2025-01-05 19:35:14 +0530

    updates

--- Models/Dungeons.cs
+++ Models/Dungeons.cs
@@ -2,6 +2,40 @@
 {
     public class Dungeons
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CombatPowerRequired { get; set; }
+        public string LevelRequirement { get; set; }
+        public string RegionDebuff { get; set; }
+        public List<string> FeaturedDrops { get; set; }
+        public List<MonsterStats> BossStats { get; set; }
+        public List<MonsterStats> MiniBossStats { get; set; }
+        public List<MonsterStats> MonstersStats { get; set; }
+    }
 
+    public class MonsterStats
+    {
+        public string Name { get; set; }
+        public string HP { get; set; }
+        public string PhysicalDefense { get; set; }
+        public string MagicalDefense { get; set; }
+        public string FireResistance { get; set; }
+        public string WaterResistance { get; set; }
+        public string NatureResistance { get; set; }
+        public string WindResistance { get; set; }
+        public string LightResistance { get; set; }
+        public string DarkResistance { get; set; }
+    }
+
+    public class  BossStats : MonsterStats
+    {
+        public string FreezeDuration { get; set; }
+        public string PetrifyDuration { get; set; }
+    }
+
+    public class MiniBossStats : MonsterStats
+    {
+        public string FreezeDuration { get; set; }
+        public string PetrifyDuration { get; set; }
     }
 }
